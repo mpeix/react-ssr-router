50c9db9ddc5f4bd2c617fe3aab6486f01db89f04
mpeix <mpeix.it@hotelbeds.com>
2020-03-17 09:34:53 +0100

    Changes webpack node enty point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve('./server/index.js'),
   target: 'node',
   externals: [nodeExternals()],
   watchOptions:{
